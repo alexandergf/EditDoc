abc1d2bd9316c90543bad1093f8eca87e1a2a845
alexandergf <alexgferreiro@hotmail.com>
2018-10-16 09:04:10 +0200

    Creem el model, i afegim el menu

--- app/src/main/java/com/example/alexandergf/editdoc/DocActivity.java
+++ app/src/main/java/com/example/alexandergf/editdoc/DocActivity.java
@@ -2,12 +2,42 @@ package com.example.alexandergf.editdoc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class DocActivity extends AppCompatActivity {
+    //model
+    private String title="Lorem Ipsum", text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend id odio vitae iaculis. Pellentesque interdum quis risus ac bibendum. In ut dignissim nisi. Suspendisse euismod elit enim, at eleifend nisi dictum ut. Sed eu vestibulum diam, et venenatis velit. Curabitur rutrum ut nisi sed commodo. Curabitur condimentum blandit ligula, a ultrices felis feugiat ut. ";
+    private TextView docview;
+    private TextView titleview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc);
+
+        titleview = findViewById(R.id.titleview);
+        docview = findViewById(R.id.docview);
+
+        titleview.setText(title);
+        docview.setText(text);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_doc,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.option_edit_title:
+                //TODO: cridar editActivity.
+
+                break;
+        }
+        return true;
     }
 }
