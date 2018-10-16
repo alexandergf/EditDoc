2b69189e72c854bc659e157f4261b324e973d52b
alexandergf <alexgferreiro@hotmail.com>
2018-10-16 09:29:53 +0200

    Pas 3: Retornem el text canviat a EditActivity.

--- app/src/main/java/com/example/alexandergf/editdoc/EditActivity.java
+++ app/src/main/java/com/example/alexandergf/editdoc/EditActivity.java
@@ -3,6 +3,7 @@ package com.example.alexandergf.editdoc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -24,5 +25,14 @@ public class EditActivity extends AppCompatActivity {
 
         edit_box.setText(text);
 
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                data.putExtra("text",edit_box.getText().toString());
+                setResult(RESULT_OK,data);
+                finish();
+            }
+        });
     }
 }
